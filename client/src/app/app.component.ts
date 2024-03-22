a7b61f67132490d40e1c44fd6436f53bb4e00ce4
Malek MSELLEM <m.msellem@hrmaps.eu.com>
2024-03-22 15:46:02 +0100

    Client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { error } from 'console';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HttpClientModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: any;
+
+  constructor(private http:HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:5146/api/users').subscribe({
+      next: response => this.users = response,
+      error : error => console.log(error),
+      complete : () => console.log('completed')
+
+    })
+   }
+}
