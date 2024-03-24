bab2dc97ad5e2f185e25d05270e8c968076c262d
Malek MSELLEM <m.msellem@hrmaps.eu.com>
2024-03-24 15:46:56 +0100

    Authentications + tokens

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,20 +1,18 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
         public UsersController(DataContext context)
         {
             _context = context;
-
         }
         [HttpGet]
         public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
@@ -22,6 +20,7 @@ namespace API.Controllers
             var users = await _context.Users.ToListAsync();
             return users;
         }
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    this.http.get('http://localhost:5146/api/users').subscribe({
+    this.http.get('https://localhost:7237/api/users').subscribe({
       next: response => this.users = response,
       error : error => console.log(error),
       complete : () => console.log('completed')
